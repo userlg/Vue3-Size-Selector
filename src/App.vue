4e207b0328d2f38feeb2511f2f75ddb24c2b2171
userlg <guipeluis90@gmail.com>
2023-01-17 19:37:47 -0400

    Working the App sizeSelector

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import SizeSelector from './components/SizeSelector.vue';
+
+</script>
+
+<template>
+  <div class="bg-amber-200 p-10 rounded border border-black shadow-md shadow-gray-500">
+    
+    <h2 class="text-2xl font-bold mb-4">Seleccione la talla {{ 'Probando' }}</h2>
+
+    <SizeSelector />
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/SizeSelector.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const possibleSizes = ['XS','S','M','L','XL','XXL'];
+
+
+
+</script>
+
+<template>
+
+<div class="flex flex-row gap-2 items-center justify-center mx-auto mb-4">
+<button v-for="( size, index ) in possibleSizes" :key="index">
+  {{ size }}
+</button>
+</div>
+   
+</template>

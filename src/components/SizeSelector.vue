1716bf7478dfe40f8ff6e4aa822227f10fa5a08e
userlg <guipeluis90@gmail.com>
2023-01-25 18:08:24 -0400

    Tests complete

--- src/components/SizeSelector.vue
+++ src/components/SizeSelector.vue
@@ -16,9 +16,9 @@ const emits = defineEmits<{
 <template>
 
     <div class="flex flex-row gap-1 sm:gap-2 items-center justify-center mx-auto">
-        <button v-for="( size, index ) in possibleSizes" :key="index" :class="{ 'active': props.selectedSize == size }"
+        <button v-for="( size, index ) in possibleSizes" v-bind:key="index" :class="{ 'active': props.selectedSize == size }"
             class="bg-white w-1/2 sm:w-12 border border-black rounded p-2" @click="emits('onChangedSize', size)">
-            <h3 class="text-center text-xs sm:text-base">{{ size }}</h3>
+            <h3 class="text-center text-xs sm:text-base"> {{ size }}</h3>
         </button>
     </div>
 
--- src/tests/appComponent.test.ts
+++ src/tests/appComponent.test.ts
@@ -1,20 +1,50 @@
-import { describe, it } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 
 import { mount } from "@vue/test-utils";
 
 import AppVue from "../App.vue";
 
-describe("AppVue Test", () =>{
- 
+describe("AppVue Test", () => {
+
     it("App component render properly", async () => {
-   
+
         const wrapper = mount(AppVue);
 
         expect(wrapper.html()).toMatchSnapshot();
 
         expect(wrapper.find("h3").text()).toEqual("M");
-    
+
     });
 
+    it("Show message h2 selector", async () => {
 
+        const wrapper = mount(AppVue);
+
+        expect(wrapper.find('h2').text()).toEqual('Choose your size');
+    });
+
+    it("Show component sizeSelector properly", async () => {
+        const wrapper = mount(AppVue);
+
+        expect(wrapper.findComponent({ name: 'SizeSelector' })).toBeTruthy();
+    });
+    
+    it("Show component sizeSelector properly", async () => {
+       
+        const wrapper = mount(AppVue);
+       
+        expect(wrapper.findComponent({ name: 'SizeSelector' })).toBeTruthy();
+    });
+
+    it("Test on Updated", async () => {
+        
+        const wrapper = mount(AppVue);
+
+        const buttons = wrapper.findComponent({ name: 'SizeSelector' }).findAll('button');
+
+        await buttons[0].trigger('click');
+
+        expect(wrapper.findComponent('onUpdated')).toBeTruthy();
+
+    });
 });
